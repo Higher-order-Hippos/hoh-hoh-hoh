ee272657f9d643ad18f3a567c4819718eca83c4b
JW Garrison <jwolfgarrison@gmail.com>
2016-12-01 18:12:45 -0500

    [#39 | MVP 0.5] Add Router. Handles Request to '/'

---
+++ client/app/app.js
@@ -0,0 +1,10 @@
+angular.module('hohhohhoh', [
+  'ngRoute'
+])
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'FILL_ME_IN',
+      controller: 'FILL_ME_IN',
+    });
+});

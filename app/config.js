dbadb533b5989cefef62df2ef171a5bb1ba4006f
Adam Wang <adamw3494@gmail.com>
2016-12-01 20:42:30 -0500

    updated knex database, controller and model

--- app/config.js
+++ app/config.js
@@ -2,35 +2,35 @@ var path = require('path');
 var knex = require('knex')({
   client: 'mysql',
   connection: {
-    filename: //TODO)
+    host: ,
+    user: hohohadmin,
+    password: '!Qaz2wsx3edc',
+    IP: 'mysqlcluster2.registeredsite.com',
+    database: 'hohohoh'
   },
-
+  useNullAsDefault: true
 });
 var db = require('bookshelf')(knex);
 
 db.knex.schema.hasTable('wishlists').then(function(exists) {
   if (!exists) {
-    db.knex.schema.createTable('wislists', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
+    db.knex.schema.createTable('wishlists', function (wishlist) {
+      wishlist.increments('id').primary();
+      wishlist.string('name', 255);
+      wishlist.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
     });
   }
 });
 
-db.knex.schema.hasTable('users').then(function(exists) {
+db.knex.schema.hasTable('items').then(function(exists) {
   if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
+    db.knex.schema.createTable('items', function (item) {
+      item.increments('id').primary();
+      item.string('name', 255);
+      item.integer('wishlistId');
+      item.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
     });

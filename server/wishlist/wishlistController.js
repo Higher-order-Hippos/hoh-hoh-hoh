79d6372638cef43bdcfeb2bfe63374f01adf235f
Sojung Park <sojungko@gmail.com>
2016-12-01 19:25:35 -0500

    [#9 MVP0.5] add server-side wishlist model and controller functions

--- server/wishlist/wishlistController.js
+++ server/wishlist/wishlistController.js
@@ -1,11 +1,27 @@
-var wishlist = require('./wishlistModel.js');
+var models = require('./wishlistModel.js');
 
 module.exports = {
 
-  getAll: function (req, res, next){
-
-  }
-
-
+  wishlists: {
+    get: function (req, res) {
+      models.wishlists.getAll(function(err, results) {
+        if (err) {
+          console.log('GET request error : ', err);
+        } else {
+          res.json(results);
+        }
+      });
+    },
+    post: function (req, res) {
+      var params = [TO_BE_FILLED]; // wishlist data being posted
+      models.wishlists.addOne(params, function(err, results) {
+        if (err) {
+          console.log('POST request error : ', err);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  },
 
 }

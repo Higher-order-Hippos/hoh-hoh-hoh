bbbb2238d1dc1111b965c568ef5b633956e430f0
Sojung Park <sojungko@gmail.com>
2016-12-01 20:20:40 -0500

    Add server-side item model and controller

--- server/wishlist/wishlistController.js
+++ server/wishlist/wishlistController.js
@@ -1,12 +1,12 @@
-var models = require('./wishlistModel.js');
+var wishlistModel = require('./wishlistModel.js');
 
 module.exports = {
 
   wishlists: {
     get: function (req, res) {
-      models.wishlists.getAll(function(err, results) {
+      wishlistModel.wishlists.getAll(function(err, results) {
         if (err) {
-          console.log('GET request error : ', err);
+          console.log('Server-side GET request error : ', err);
         } else {
           res.json(results);
         }
@@ -14,9 +14,9 @@ module.exports = {
     },
     post: function (req, res) {
       var params = [FILL_ME_IN]; // TODO whatever wishlist data that are being posted
-      models.wishlists.addOne(params, function(err, results) {
+      wishlistModel.wishlists.addOne(params, function(err, results) {
         if (err) {
-          console.log('POST request error : ', err);
+          console.log('Server-side POST request error : ', err);
         } else {
           res.sendStatus(201);
         }

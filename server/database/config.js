460051321c7af1705dc7671d006c68bdea918d42
Adam Wang <adamw3494@gmail.com>
2016-12-02 10:51:59 -0500

    correction on config.js file, added user schema

--- server/database/config.js
+++ server/database/config.js
@@ -2,10 +2,9 @@ var path = require('path');
 var knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: 'localhost',
+    host: 'mysqlcluster2.registeredsite.com',
     user: 'hohohadmin',
     password: '!Qaz2wsx3edc',
-    IP: 'mysqlcluster2.registeredsite.com',
     database: 'hohohoh'
   },
   useNullAsDefault: true
@@ -17,6 +16,7 @@ db.knex.schema.hasTable('wishlists').then(function(exists) {
     db.knex.schema.createTable('wishlists', function (wishlist) {
       wishlist.increments('id').primary();
       wishlist.string('name', 255);
+      wishlist.integer('userId');
       wishlist.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
@@ -31,6 +31,20 @@ db.knex.schema.hasTable('items').then(function(exists) {
       item.string('name', 255);
       item.integer('wishlistId');
       item.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 100);
+      user.string('password', 100);
+      user.string('name', 150);
+      user.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
     });

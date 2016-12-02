a0d12130de2773fb7366fd970d940bac2dac7fa1
JW Garrison <jwolfgarrison@gmail.com>
2016-12-01 20:01:45 -0500

    [#6|MVP0.5] Update getWishlist's POST request URL

--- server/routing.js
+++ server/routing.js
@@ -1,12 +1,14 @@
 var bodyParser = require('body-parser');
 var path = require('path');
+var wishlistController = require('./wishlist/wishlistController');
 
-module.exports = function(app, express) {
-  app.use(bodyParser.urlencoded({extended: true}));
+// TODO: Require wishlistController here.
+
+module.exports = function (app, express) {
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-  app.use(express.static(path.join(__dirname, '/../client'));
+  app.use(express.static(path.join(__dirname, '/../client')));
+  app.use(express.static(path.join(__dirname, '/../node_modules')));
 
-  app.get('/wishlist', wishlistController.getAll);
-
-
-}
+  app.get('/api/wishlist', wishlistController.getAll);
+};

ea7a506cc9003e23f6e8de99c36dbd76cd4f111b
JW Garrison <jwolfgarrison@gmail.com>
2016-12-06 21:08:03 -0500

    [Refator] White space

--- server/routing.js
+++ server/routing.js
@@ -10,14 +10,6 @@ module.exports = (app, express) => {
   app.use(express.static(path.join(__dirname, '/../client')));
   app.use(express.static(path.join(__dirname, '/../client/app')));
   app.use(express.static(path.join(__dirname, '/../node_modules')));
-  app.use(cookieParser('shhhh, very secret'));
-
-  // required for passport
-  app.use(session({
-    secret: 'hohlife',
-    resave: false,
-    saveUninitialized: false,
-  }));
 
   // requests for home page, with auth check
   app.post('/api/users/signin', userController.users.signin);

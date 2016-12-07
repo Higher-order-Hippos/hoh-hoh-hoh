4a44430c7b9bf2723aaa7183e743bb8228a8f140
JW Garrison <jwolfgarrison@gmail.com>
2016-12-07 16:46:36 -0500

    [Refactor] Routing.js

--- server/routing.js
+++ server/routing.js
@@ -4,21 +4,12 @@ const userController = require('./user/userController');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-
 module.exports = (app, express) => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(express.static(path.join(__dirname, '/../client')));
   app.use(express.static(path.join(__dirname, '/../client/app')));
   app.use(express.static(path.join(__dirname, '/../node_modules')));
-  app.use(cookieParser('shhhh, very secret'));
-
-  // required for passport
-  app.use(session({
-    secret: 'hohlife',
-    resave: false,
-    saveUninitialized: false,
-  }));
 
   // requests for home page, with auth check
   app.post('/api/users/signin', userController.users.signin);

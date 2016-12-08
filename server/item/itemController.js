511c4ab8d5de81aa03183b0c10e2eef41ff80fb9
Adam Wang <adamw3494@gmail.com>
2016-12-07 19:16:05 -0500

    fixed documentation

--- server/item/itemController.js
+++ server/item/itemController.js
@@ -2,28 +2,33 @@ const itemModel = require('./itemModel');
 
 module.exports = {
   items: {
-    get({ body: { id } }, res) {      //request in object format, plucked out userId from req.body, passing down as params
+    get({ body: { id } }, res) {
+      //request in object format, plucked out userId from req.body, passing down as params
+      //send back item results in json format
       const params = id;
       itemModel.items.getAll(params, (results) => {
-        res.json(results);            //send back item results in json format
+        res.json(results);
       });
     },
 
-    post({ body: { name, id } }, res) {   //request in object format, plucked out itemname and userId from req.body, passing down as params
+    post({ body: { name, id } }, res) {
+      //request in object format, plucked out itemname and userId from req.body, passing down as params
       const params = [name, id];
       itemModel.items.addOne(params, () => {
         res.sendStatus(201);
       });
     },
 
-    rename({ body: { name, item } }, res) {  //request in object format, plucked out itemname and userId from req.body, passing down as params
+    rename({ body: { name, item } }, res) {
+      //request in object format, plucked out itemname and userId from req.body, passing down as params
       const params = [name, item];
       itemModel.items.renameItem(params, () => {
         res.sendStatus(201);
       });
     },
 
-    delete({ body: { itemId } }, res) {   //request in object format, plucked out itemId from req.body object, passing down as params
+    delete({ body: { itemId } }, res) {
+      //request in object format, plucked out itemId from req.body object, passing down as params
       const params = itemId;
       itemModel.items.deleteItem(params, () => {
         res.sendStatus(201);

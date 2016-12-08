2fae903f5700fb163c39542116923f12698589a4
Adam Wang <adamw3494@gmail.com>
2016-12-07 19:05:54 -0500

    added documentation

--- server/database/config_deploy.js
+++ server/database/config_deploy.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-const pw = process.env.key || require('./pw.js');
+const pw = process.env.key || require('./pw.js');  //pw stored on heroku as config var, contact HoH for access
 
 const connection = mysql.createConnection({
   host: 'mysqlcluster2.registeredsite.com',
--- server/item/itemController.js
+++ server/item/itemController.js
@@ -2,28 +2,28 @@ const itemModel = require('./itemModel');
 
 module.exports = {
   items: {
-    get({ body: { id } }, res) {
+    get({ body: { id } }, res) {      //request in object format, plucked out userId from req.body, passing down as params
       const params = id;
       itemModel.items.getAll(params, (results) => {
-        res.json(results);
+        res.json(results);            //send back item results in json format
       });
     },
 
-    post({ body: { name, id } }, res) {
+    post({ body: { name, id } }, res) {   //request in object format, plucked out itemname and userId from req.body, passing down as params
       const params = [name, id];
       itemModel.items.addOne(params, () => {
         res.sendStatus(201);
       });
     },
 
-    rename({ body: { name, item } }, res) {
+    rename({ body: { name, item } }, res) {  //request in object format, plucked out itemname and userId from req.body, passing down as params
       const params = [name, item];
       itemModel.items.renameItem(params, () => {
         res.sendStatus(201);
       });
     },
 
-    delete({ body: { itemId } }, res) {
+    delete({ body: { itemId } }, res) {   //request in object format, plucked out itemId from req.body object, passing down as params
       const params = itemId;
       itemModel.items.deleteItem(params, () => {
         res.sendStatus(201);

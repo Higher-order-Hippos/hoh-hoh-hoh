3cf85ecaa370827b43ddc8f28ed64a922f57169a
Yimothy <tim@Yimothys-MacBook-Pro.local>
2016-12-08 16:01:37 -0500

    Working on grabbing santa-main angular

--- client/app/santa/santa.js
+++ client/app/santa/santa.js
@@ -1,9 +1,24 @@
 angular.module('hoh.santa', [])
 
 .controller('SantaController', function($scope, SantaFactory) {
+  $scope.data = {};
+  $scope.data.roomUsers = ['USER1', 'USER2'];
+
+  $scope.addUserToRoom = function(user) {
+    $scope.data.roomUsers.push(user);
+  }
 
 })
 
 .factory('SantaFactory', function($http){
+//Creates room and save in user's database
 
+//Gets all rooms for the user from database
+
+//Gets all users in the room
+
+//Gets the user's receiver
+
+
+  //return methods in object
 })

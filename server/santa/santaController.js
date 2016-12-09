6c206a1c38e33d798549463aaee6d0b9dc92f7f7
Yimothy <tim@Yimothys-MacBook-Pro.local>
2016-12-09 16:12:39 -0500

    Added create room functions to controller and model

--- server/santa/santaController.js
+++ server/santa/santaController.js
@@ -1,1 +1,11 @@
 var santaModel = require('./santaModel');
+
+module.exports = {
+  createRoom: function(req, res) {
+    console.log('THIS IS REQ.BODY IN SANTACONTROLLER DB: ',req.body);
+    var params = req.body.data;
+    santaModel.createRoom(params, () => {
+      res.sendStatus(201);
+    });
+  }
+}
